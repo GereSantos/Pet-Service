96eff4245d8ca92667b81353e4b0c13809c83fed
Geremias Santos <Geremias99999@gmail.com>
2022-06-22 15:03:47 -0300

    Pet Service New Features

--- AnimalApp/Program.cs
+++ AnimalApp/Program.cs
@@ -49,7 +49,47 @@ namespace AnimalApp
                     Console.WriteLine("3 - Cinomose canina");
                     Console.WriteLine("4 - Outro(Qual?)");
                     int tratAnimal = int.Parse(Console.ReadLine());
-                    
+                    if(tratAnimal == 1)
+                    {
+                        Console.WriteLine("Tratamento selecionado Parvovirose");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.Clear();
+                        Console.WriteLine("Atendimento marcado para " + DateTime.UtcNow);
+                        Console.WriteLine("Obrigado por contar conosco " + Nome + ", Estamos a diposição!");
+                    }else if(tratAnimal == 2)
+                    {
+                        Console.WriteLine("Tratamento selecionado Otite");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.Clear();
+                        Console.WriteLine("Atendimento marcado para " + DateTime.UtcNow);
+                        Console.WriteLine("Obrigado por contar conosco " + Nome + ", Estamos a diposição!");
+                    }
+                    else if (tratAnimal == 3)
+                    {
+                        Console.WriteLine("Tratamento selecionado Cinomose canina");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.Clear();
+                        Console.WriteLine("Atendimento marcado para " + DateTime.UtcNow);
+                        Console.WriteLine("Obrigado por contar conosco " + Nome + ", Estamos a diposição!");
+                    }
+                    else if (tratAnimal == 4)
+                    {
+                        Console.WriteLine("Qual tratamento pretende realizar?");
+                        string trat = Console.ReadLine();
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.Clear();
+                        Console.WriteLine("Atendimento marcado " + "de " + trat +", para dia e horário " + DateTime.UtcNow);
+                        Console.WriteLine("Obrigado por contar conosco ," + Nome + ", Estamos a diposição!");
+                    }
+
                 }
                 else if(chAnimal == "Gato" || chAnimal == "gato")
                 {
@@ -68,8 +108,47 @@ namespace AnimalApp
                     Console.WriteLine("3 - Insuficiência renal");
                     Console.WriteLine("4 - Outro(Qual?)");
                     int tratAnimal = int.Parse(Console.ReadLine());
-                    
+                    if (tratAnimal == 1)
+                    {
+                        Console.WriteLine("Tratamento selecionado FIV (Imunodeficiência Viral Felina) ");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.Clear();
+                        Console.WriteLine("Atendimento marcado para " + DateTime.UtcNow);
+                        Console.WriteLine("Obrigado por contar conosco " + Nome + ", Estamos a diposição!");
+                    }else if(tratAnimal == 2)
+                    {
+                        Console.WriteLine("Tratamento selecionado FELV (Leucemia Viral Felina) ");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.Clear();
+                        Console.WriteLine("Atendimento marcado para " + DateTime.UtcNow);
+                        Console.WriteLine("Obrigado por contar conosco " + Nome + ", Estamos a diposição!");
+                    }
+                    else if (tratAnimal == 3)
+                    {
+                        Console.WriteLine("Tratamento selecionado Insuficiência renal ");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.Clear();
+                        Console.WriteLine("Atendimento marcado para " + DateTime.UtcNow);
+                        Console.WriteLine("Obrigado por contar conosco " + Nome + ", Estamos a diposição!");
+                    }else if(tratAnimal == 4)
+                    {
+                        Console.WriteLine("Qual tratamento pretende realizar?");
+                        string trat = Console.ReadLine();
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.Clear();
+                        Console.WriteLine("Atendimento marcado " + "de " + trat + ", para dia e horário " + DateTime.UtcNow);
+                        Console.WriteLine("Obrigado por contar conosco ," + Nome + ", Estamos a diposição!");
+                    }
                 }
+
 
             }
             else if(choice == 'N' || choice == 'n')
@@ -77,6 +156,139 @@ namespace AnimalApp
                 Console.WriteLine("Certo, iremos criar gerar um número e essa será a sua senha..");
                 int password = randNumber.Next();
                 Console.WriteLine("Sua senha de atendimento é: "  +  password);
+                Console.WriteLine("Aguarde alguns instantes que iremos te redirecionar para o agendamento do atendimento");
+                int time = 20;
+                for(int i = 20; i >0; i--)
+                {
+                    time = time - 1;
+                    Console.WriteLine($"Aguarde um momento: {i}" ) ;
+                }
+                Console.WriteLine("Tempo restante: " + time);
+                
+                Console.WriteLine("Devo te chamar de..? ");
+                string name = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("Certo, " + name + ", precisamos agora saber que tipo de animal você quer tratar..");
+                string chAnimal = Console.ReadLine();
+                if (chAnimal == "Cachorro" || chAnimal == "cachorro")
+                {
+                    Console.WriteLine($"Vejo que selecionou {chAnimal}");
+                    Console.Write("Nome do animal: ");
+                    string animalName = Console.ReadLine();
+                    Console.Write("Idade do animal: (apenas número) ");
+                    int idadeAnimal = int.Parse(Console.ReadLine());
+                    Console.Write("Data da última vacina do Animal: ");
+                    DateTime vacina = DateTime.Parse(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine("Qual tipo de tratamento o animal necessita? ");
+                    Console.WriteLine("1 - Parvovirose");
+                    Console.WriteLine("2 - Otite");
+                    Console.WriteLine("3 - Cinomose canina");
+                    Console.WriteLine("4 - Outro(Qual?)");
+                    int tratAnimal = int.Parse(Console.ReadLine());
+                    if (tratAnimal == 1)
+                    {
+                        Console.WriteLine("Tratamento selecionado Parvovirose");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.Clear();
+                        Console.WriteLine("Atendimento marcado para " + DateTime.UtcNow);
+                        Console.WriteLine("Obrigado por contar conosco " + name + ", Estamos a diposição!");
+                    }
+                    else if (tratAnimal == 2)
+                    {
+                        Console.WriteLine("Tratamento selecionado Otite");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.Clear();
+                        Console.WriteLine("Atendimento marcado para " + DateTime.UtcNow);
+                        Console.WriteLine("Obrigado por contar conosco " + name + ", Estamos a diposição!");
+                    }
+                    else if (tratAnimal == 3)
+                    {
+                        Console.WriteLine("Tratamento selecionado Cinomose canina");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.Clear();
+                        Console.WriteLine("Atendimento marcado para " + DateTime.UtcNow);
+                        Console.WriteLine("Obrigado por contar conosco " + name + ", Estamos a diposição!");
+                    }
+                    else if (tratAnimal == 4)
+                    {
+                        Console.WriteLine("Qual tratamento pretende realizar?");
+                        string trat = Console.ReadLine();
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.Clear();
+                        Console.WriteLine("Atendimento marcado " + "de " + trat + ", para dia e horário " + DateTime.UtcNow);
+                        Console.WriteLine("Obrigado por contar conosco ," + name + ", Estamos a diposição!");
+                    }
+
+                }
+                else if (chAnimal == "Gato" || chAnimal == "gato")
+                {
+
+                    Console.WriteLine($"Vejo que selecionou {chAnimal}");
+                    Console.Write("Nome do animal: ");
+                    string animalName = Console.ReadLine();
+                    Console.Write("Idade do animal: (apenas número) ");
+                    int idadeAnimal = int.Parse(Console.ReadLine());
+                    Console.Write("Data da última vacina do Animal: ");
+                    DateTime vacina = DateTime.Parse(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine("Qual tipo de tratamento o animal necessita? ");
+                    Console.WriteLine("1 - FIV (Imunodeficiência Viral Felina) ");
+                    Console.WriteLine("2 - FELV (Leucemia Viral Felina)");
+                    Console.WriteLine("3 - Insuficiência renal");
+                    Console.WriteLine("4 - Outro(Qual?)");
+                    int tratAnimal = int.Parse(Console.ReadLine());
+                    if (tratAnimal == 1)
+                    {
+                        Console.WriteLine("Tratamento selecionado FIV (Imunodeficiência Viral Felina) ");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.Clear();
+                        Console.WriteLine("Atendimento marcado para " + DateTime.UtcNow);
+                        Console.WriteLine("Obrigado por contar conosco " + name + ", Estamos a diposição!");
+                    }
+                    else if (tratAnimal == 2)
+                    {
+                        Console.WriteLine("Tratamento selecionado FELV (Leucemia Viral Felina) ");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.Clear();
+                        Console.WriteLine("Atendimento marcado para " + DateTime.UtcNow);
+                        Console.WriteLine("Obrigado por contar conosco " + name + ", Estamos a diposição!");
+                    }
+                    else if (tratAnimal == 3)
+                    {
+                        Console.WriteLine("Tratamento selecionado Insuficiência renal ");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.Clear();
+                        Console.WriteLine("Atendimento marcado para " + DateTime.UtcNow);
+                        Console.WriteLine("Obrigado por contar conosco " + name + ", Estamos a diposição!");
+                    }
+                    else if (tratAnimal == 4)
+                    {
+                        Console.WriteLine("Qual tratamento pretende realizar?");
+                        string trat = Console.ReadLine();
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.WriteLine("Gerando data...");
+                        Console.Clear();
+                        Console.WriteLine("Atendimento marcado " + "de " + trat + ", para dia e horário " + DateTime.UtcNow);
+                        Console.WriteLine("Obrigado por contar conosco ," + name + ", Estamos a diposição!");
+                    }
+                }
+
             }
             else
             {
